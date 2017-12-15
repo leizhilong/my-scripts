5ac4de7db751c197b7e53f6377136f0d6c7cebf8
Zhilong Lei <zllei@sysnew.com>
2017-12-15 23:41:47 +0800

    update vscode repo

--- fedora-init-script/run.sh
+++ fedora-init-script/run.sh
@@ -8,14 +8,16 @@ else
     sudo cp ./yum.repos.d/google-chrome-64bit.repo /etc/yum.repos.d/
 fi
 
- sudo cp ./yum.repos.d/vscode.repo /etc/yum.repos.d/
+# add vscode repo
+sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
+sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
 
 
 # install tools which are usually used in working
 sudo dnf install ctags ack dos2unix cvs svn  \
 	git gitg htop tig  tuxcmd terminator \
 	wireshark telnet httpd \
-	docker golang \
+	docker golang code \
 	fcitx fcitx-configtool fcitx-sunpinyin vim meld \
 	google-chrome-stable thunderbird samba \
 	gcc gcc-c++ expect -y 
