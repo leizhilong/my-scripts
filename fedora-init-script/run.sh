3f305bd45a80131a224f6dc409c85cfb5b900784
Zhilong Lei <leizhilong@me.com>
2020-04-16 11:29:47 +0800

    update fedora 31 scripts

--- fedora-init-script/run.sh
+++ fedora-init-script/run.sh
@@ -1,41 +1,43 @@
 ##########################install###############################################
-
-# add google-chrome repo
-bit=`uname -i`
-if [ "i386" == "$bit" ];then
-    sudo cp ./yum.repos.d/google-chrome-32bit.repo /etc/yum.repos.d/
-else
-    sudo cp ./yum.repos.d/google-chrome-64bit.repo /etc/yum.repos.d/
-fi
+dnf erase libreoffice* qemu* libvirt* -y
+dnf update -y
+dnf install fedora-workstation-repositories -y
+dnf config-manager --set-enabled google-chrome
 
 # add vscode repo
-sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
-sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
+rpm --import https://packages.microsoft.com/keys/microsoft.asc
+sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
 
+dnf check-update
 
 # install tools which are usually used in working
-sudo dnf install ctags ack dos2unix cvs svn  \
+dnf install ctags ack dos2unix cvs svn  \
 	git gitg htop tig zsh tuxcmd terminator \
 	wireshark telnet httpd filezilla \
-	docker golang code pacmanager\
-	fcitx fcitx-configtool fcitx-sunpinyin vim meld \
+	golang rust nodejs pacmanager \
+	vim code meld \
 	google-chrome-stable thunderbird samba \
-	gcc gcc-c++ expect -y 
+	gcc gcc-c++ expect the_silver_searcher -y
 
 ##########################config###############################################
 
 # disable selinux
-sudo sed -i 's@SELINUX=enforcing@SELINUX=disabled@g' /etc/selinux/config
-sudo setenforce 0
+sed -i 's@SELINUX=enforcing@SELINUX=disabled@g' /etc/selinux/config
+setenforce 0
 
 # close unuseful services
-sudo systemctl disable firewalld && sudo systemctl stop firewalld 
+systemctl disable firewalld && sudo systemctl stop firewalld
 
 # git config
-. ./git-config.sh
-
-##########################update###############################################
-
+git config --global user.name "Zhilong Lei"
+git config --global user.email "leizhilong@me.com"
+git config --global color.ui true
+git config --global alias.co checkout
+git config --global alias.ci commit
+git config --global alias.st status
+git config --global alias.br branch
+git config --global alias.up "pull --rebase"
+git config branch.master.mergeoptions --ff-only
 
 ###############################################################################
 
@@ -44,10 +46,8 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/mas
 cp ./zshrc ~/.zshrc
 
 # hosts
-sudo cp ./update-hosts.sh /usr/bin/ && sudo chmod +x /usr/bin/update-hosts.sh
-sudo sh -c /usr/bin/update-hosts.sh
+cp ./update-hosts.sh /usr/bin/ && sudo chmod +x /usr/bin/update-hosts.sh
+sh -c /usr/bin/update-hosts.sh
 echo "/etc/hosts file lines = `cat /etc/hosts | wc -l`"
-sudo crontab -u root ./crontab/updatehosts
+crontab -u root ./crontab/updatehosts
 
-# udpate
-sudo yum update -y
